df5905d985de85ee712dcbb75fb700953b1898eb
Kyle Brophy <kyle.brophy@sympatico.ca>
2016-08-04 01:18:50 -0400

    Update to 2.3.3

--- ComponentSettings.designer.cs
+++ ComponentSettings.designer.cs
@@ -131,7 +131,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelP1A.AutoSize = true;
             this.labelP1A.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelP1A.Location = new System.Drawing.Point(8, 3);
+            this.labelP1A.Location = new System.Drawing.Point(6, 3);
             this.labelP1A.Name = "labelP1A";
             this.labelP1A.Size = new System.Drawing.Size(105, 13);
             this.labelP1A.TabIndex = 26;
@@ -143,7 +143,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelP1B.AutoSize = true;
             this.labelP1B.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelP1B.Location = new System.Drawing.Point(227, 3);
+            this.labelP1B.Location = new System.Drawing.Point(219, 3);
             this.labelP1B.Name = "labelP1B";
             this.labelP1B.Size = new System.Drawing.Size(64, 13);
             this.labelP1B.TabIndex = 27;
@@ -155,7 +155,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelP2A.AutoSize = true;
             this.labelP2A.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelP2A.Location = new System.Drawing.Point(8, 3);
+            this.labelP2A.Location = new System.Drawing.Point(6, 3);
             this.labelP2A.Name = "labelP2A";
             this.labelP2A.Size = new System.Drawing.Size(63, 13);
             this.labelP2A.TabIndex = 11;
@@ -167,7 +167,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelP2B.AutoSize = true;
             this.labelP2B.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelP2B.Location = new System.Drawing.Point(227, 3);
+            this.labelP2B.Location = new System.Drawing.Point(219, 3);
             this.labelP2B.Name = "labelP2B";
             this.labelP2B.Size = new System.Drawing.Size(36, 13);
             this.labelP2B.TabIndex = 11;
@@ -667,24 +667,24 @@
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.999999F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.999999F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(441, 293);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(429, 293);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
             // txt_Shop
             // 
             this.txt_Shop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_Shop.Location = new System.Drawing.Point(311, 4);
+            this.txt_Shop.Location = new System.Drawing.Point(301, 4);
             this.txt_Shop.Name = "txt_Shop";
-            this.txt_Shop.Size = new System.Drawing.Size(127, 20);
+            this.txt_Shop.Size = new System.Drawing.Size(125, 20);
             this.txt_Shop.TabIndex = 14;
             // 
             // lbl_Shop
             // 
             this.lbl_Shop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_Shop.AutoSize = true;
-            this.lbl_Shop.Location = new System.Drawing.Point(223, 8);
+            this.lbl_Shop.Location = new System.Drawing.Point(216, 8);
             this.lbl_Shop.Name = "lbl_Shop";
-            this.lbl_Shop.Size = new System.Drawing.Size(82, 13);
+            this.lbl_Shop.Size = new System.Drawing.Size(79, 13);
             this.lbl_Shop.TabIndex = 13;
             this.lbl_Shop.Text = "Shoplifting:";
             // 
@@ -694,16 +694,16 @@
             this.lbl_TK.AutoSize = true;
             this.lbl_TK.Location = new System.Drawing.Point(3, 8);
             this.lbl_TK.Name = "lbl_TK";
-            this.lbl_TK.Size = new System.Drawing.Size(82, 13);
+            this.lbl_TK.Size = new System.Drawing.Size(79, 13);
             this.lbl_TK.TabIndex = 11;
             this.lbl_TK.Text = "Tail Key:";
             // 
             // txt_TK
             // 
             this.txt_TK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_TK.Location = new System.Drawing.Point(91, 4);
+            this.txt_TK.Location = new System.Drawing.Point(88, 4);
             this.txt_TK.Name = "txt_TK";
-            this.txt_TK.Size = new System.Drawing.Size(126, 20);
+            this.txt_TK.Size = new System.Drawing.Size(122, 20);
             this.txt_TK.TabIndex = 12;
             // 
             // lbl_BK
@@ -712,16 +712,16 @@
             this.lbl_BK.AutoSize = true;
             this.lbl_BK.Location = new System.Drawing.Point(3, 37);
             this.lbl_BK.Name = "lbl_BK";
-            this.lbl_BK.Size = new System.Drawing.Size(82, 13);
+            this.lbl_BK.Size = new System.Drawing.Size(79, 13);
             this.lbl_BK.TabIndex = 15;
             this.lbl_BK.Text = "Bird Key:";
             // 
             // txt_BK
             // 
             this.txt_BK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_BK.Location = new System.Drawing.Point(91, 33);
+            this.txt_BK.Location = new System.Drawing.Point(88, 33);
             this.txt_BK.Name = "txt_BK";
-            this.txt_BK.Size = new System.Drawing.Size(126, 20);
+            this.txt_BK.Size = new System.Drawing.Size(122, 20);
             this.txt_BK.TabIndex = 16;
             // 
             // lbl_Flips
@@ -730,34 +730,34 @@
             this.lbl_Flips.AutoSize = true;
             this.lbl_Flips.Location = new System.Drawing.Point(3, 66);
             this.lbl_Flips.Name = "lbl_Flips";
-            this.lbl_Flips.Size = new System.Drawing.Size(82, 13);
+            this.lbl_Flips.Size = new System.Drawing.Size(79, 13);
             this.lbl_Flips.TabIndex = 18;
             this.lbl_Flips.Text = "Flippers:";
             // 
             // txt_Flips
             // 
             this.txt_Flips.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_Flips.Location = new System.Drawing.Point(91, 62);
+            this.txt_Flips.Location = new System.Drawing.Point(88, 62);
             this.txt_Flips.Name = "txt_Flips";
-            this.txt_Flips.Size = new System.Drawing.Size(126, 20);
+            this.txt_Flips.Size = new System.Drawing.Size(122, 20);
             this.txt_Flips.TabIndex = 19;
             // 
             // lbl_Marin
             // 
             this.lbl_Marin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_Marin.AutoSize = true;
-            this.lbl_Marin.Location = new System.Drawing.Point(223, 37);
+            this.lbl_Marin.Location = new System.Drawing.Point(216, 37);
             this.lbl_Marin.Name = "lbl_Marin";
-            this.lbl_Marin.Size = new System.Drawing.Size(82, 13);
+            this.lbl_Marin.Size = new System.Drawing.Size(79, 13);
             this.lbl_Marin.TabIndex = 6;
             this.lbl_Marin.Text = "Get Marin:";
             // 
             // txt_Marin
             // 
             this.txt_Marin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_Marin.Location = new System.Drawing.Point(311, 33);
+            this.txt_Marin.Location = new System.Drawing.Point(301, 33);
             this.txt_Marin.Name = "txt_Marin";
-            this.txt_Marin.Size = new System.Drawing.Size(127, 20);
+            this.txt_Marin.Size = new System.Drawing.Size(125, 20);
             this.txt_Marin.TabIndex = 1;
             // 
             // lbl_ML
@@ -766,16 +766,16 @@
             this.lbl_ML.AutoSize = true;
             this.lbl_ML.Location = new System.Drawing.Point(3, 88);
             this.lbl_ML.Name = "lbl_ML";
-            this.lbl_ML.Size = new System.Drawing.Size(82, 26);
+            this.lbl_ML.Size = new System.Drawing.Size(79, 26);
             this.lbl_ML.TabIndex = 6;
             this.lbl_ML.Text = "Magnifying Lens:";
             // 
             // txt_ML
             // 
             this.txt_ML.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_ML.Location = new System.Drawing.Point(91, 91);
+            this.txt_ML.Location = new System.Drawing.Point(88, 91);
             this.txt_ML.Name = "txt_ML";
-            this.txt_ML.Size = new System.Drawing.Size(126, 20);
+            this.txt_ML.Size = new System.Drawing.Size(122, 20);
             this.txt_ML.TabIndex = 8;
             // 
             // lbl_L1Sword
@@ -784,16 +784,16 @@
             this.lbl_L1Sword.AutoSize = true;
             this.lbl_L1Sword.Location = new System.Drawing.Point(3, 124);
             this.lbl_L1Sword.Name = "lbl_L1Sword";
-            this.lbl_L1Sword.Size = new System.Drawing.Size(82, 13);
+            this.lbl_L1Sword.Size = new System.Drawing.Size(79, 13);
             this.lbl_L1Sword.TabIndex = 6;
             this.lbl_L1Sword.Text = "L1 Sword:";
             // 
             // txt_L1Sword
             // 
             this.txt_L1Sword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_L1Sword.Location = new System.Drawing.Point(91, 120);
+            this.txt_L1Sword.Location = new System.Drawing.Point(88, 120);
             this.txt_L1Sword.Name = "txt_L1Sword";
-            this.txt_L1Sword.Size = new System.Drawing.Size(126, 20);
+            this.txt_L1Sword.Size = new System.Drawing.Size(122, 20);
             this.txt_L1Sword.TabIndex = 9;
             // 
             // lbl_L2Sword
@@ -802,88 +802,88 @@
             this.lbl_L2Sword.AutoSize = true;
             this.lbl_L2Sword.Location = new System.Drawing.Point(3, 153);
             this.lbl_L2Sword.Name = "lbl_L2Sword";
-            this.lbl_L2Sword.Size = new System.Drawing.Size(82, 13);
+            this.lbl_L2Sword.Size = new System.Drawing.Size(79, 13);
             this.lbl_L2Sword.TabIndex = 6;
             this.lbl_L2Sword.Text = "L2 Sword:";
             // 
             // txt_L2Sword
             // 
             this.txt_L2Sword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_L2Sword.Location = new System.Drawing.Point(91, 149);
+            this.txt_L2Sword.Location = new System.Drawing.Point(88, 149);
             this.txt_L2Sword.Name = "txt_L2Sword";
-            this.txt_L2Sword.Size = new System.Drawing.Size(126, 20);
+            this.txt_L2Sword.Size = new System.Drawing.Size(122, 20);
             this.txt_L2Sword.TabIndex = 10;
             // 
             // lbl_RP
             // 
             this.lbl_RP.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_RP.AutoSize = true;
-            this.lbl_RP.Location = new System.Drawing.Point(223, 66);
+            this.lbl_RP.Location = new System.Drawing.Point(216, 66);
             this.lbl_RP.Name = "lbl_RP";
-            this.lbl_RP.Size = new System.Drawing.Size(82, 13);
+            this.lbl_RP.Size = new System.Drawing.Size(79, 13);
             this.lbl_RP.TabIndex = 6;
             this.lbl_RP.Text = "Rooster Photo:";
             // 
             // txt_RP
             // 
             this.txt_RP.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_RP.Location = new System.Drawing.Point(311, 62);
+            this.txt_RP.Location = new System.Drawing.Point(301, 62);
             this.txt_RP.Name = "txt_RP";
-            this.txt_RP.Size = new System.Drawing.Size(127, 20);
+            this.txt_RP.Size = new System.Drawing.Size(125, 20);
             this.txt_RP.TabIndex = 2;
             // 
             // lbl_Song1
             // 
             this.lbl_Song1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_Song1.AutoSize = true;
-            this.lbl_Song1.Location = new System.Drawing.Point(223, 95);
+            this.lbl_Song1.Location = new System.Drawing.Point(216, 95);
             this.lbl_Song1.Name = "lbl_Song1";
-            this.lbl_Song1.Size = new System.Drawing.Size(82, 13);
+            this.lbl_Song1.Size = new System.Drawing.Size(79, 13);
             this.lbl_Song1.TabIndex = 6;
             this.lbl_Song1.Text = "Marin\'s Song:";
             // 
             // txt_Song1
             // 
             this.txt_Song1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_Song1.Location = new System.Drawing.Point(311, 91);
+            this.txt_Song1.Location = new System.Drawing.Point(301, 91);
             this.txt_Song1.Name = "txt_Song1";
-            this.txt_Song1.Size = new System.Drawing.Size(127, 20);
+            this.txt_Song1.Size = new System.Drawing.Size(125, 20);
             this.txt_Song1.TabIndex = 3;
             // 
             // lbl_Song2
             // 
             this.lbl_Song2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_Song2.AutoSize = true;
-            this.lbl_Song2.Location = new System.Drawing.Point(223, 117);
+            this.lbl_Song2.Location = new System.Drawing.Point(216, 117);
             this.lbl_Song2.Name = "lbl_Song2";
-            this.lbl_Song2.Size = new System.Drawing.Size(82, 26);
+            this.lbl_Song2.Size = new System.Drawing.Size(79, 26);
             this.lbl_Song2.TabIndex = 6;
             this.lbl_Song2.Text = "Manbo\'s Mambo:";
             // 
             // txt_Song2
             // 
             this.txt_Song2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_Song2.Location = new System.Drawing.Point(311, 120);
+            this.txt_Song2.Location = new System.Drawing.Point(301, 120);
             this.txt_Song2.Name = "txt_Song2";
-            this.txt_Song2.Size = new System.Drawing.Size(127, 20);
+            this.txt_Song2.Size = new System.Drawing.Size(125, 20);
             this.txt_Song2.TabIndex = 4;
             // 
             // lbl_Song3
             // 
             this.lbl_Song3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_Song3.AutoSize = true;
-            this.lbl_Song3.Location = new System.Drawing.Point(223, 153);
+            this.lbl_Song3.Location = new System.Drawing.Point(216, 153);
             this.lbl_Song3.Name = "lbl_Song3";
-            this.lbl_Song3.Size = new System.Drawing.Size(82, 13);
+            this.lbl_Song3.Size = new System.Drawing.Size(79, 13);
             this.lbl_Song3.TabIndex = 7;
             this.lbl_Song3.Text = "Frog Song:";
             // 
             // txt_Song3
             // 
             this.txt_Song3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_Song3.Location = new System.Drawing.Point(311, 149);
+            this.txt_Song3.Location = new System.Drawing.Point(301, 149);
             this.txt_Song3.Name = "txt_Song3";
-            this.txt_Song3.Size = new System.Drawing.Size(127, 20);
+            this.txt_Song3.Size = new System.Drawing.Size(125, 20);
             this.txt_Song3.TabIndex = 5;
             // 
             // chkStartTimer
